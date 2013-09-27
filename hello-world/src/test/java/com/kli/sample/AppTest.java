e63b0efbb2b797c060d02da745d34031cd840a54
Kevin Li <gzkevinl@yahoo.com>
2013-09-26 21:03:36 -0400

    Initial Version

---
+++ hello-world/src/test/java/com/kli/sample/AppTest.java
@@ -0,0 +1,15 @@
+package com.kli.sample;
+import org.junit.Assert; 
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    @Test
+    public void testApp()
+    {
+        Assert.assertTrue( true );
+    }
+}
